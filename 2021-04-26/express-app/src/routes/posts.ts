9c5442c75ce033b24e9bc43949d4e0d4c6550397
davidTorresTrejo <Luis.Torres@ejamerica.com>
2021-04-29 11:59:54 -0500

    2021-04-29 Express-App Connect With Postgres
    Add necesary files & configs to connect Express-App with Postgres.
    
    Create express-app database in Postgres
    Create posts and users entityes for models.

--- 2021-04-26/express-app/src/routes/posts.ts
+++ 2021-04-26/express-app/src/routes/posts.ts
@@ -1,19 +1,25 @@
 import express from 'express';
+import {getRepository} from 'typeorm';
+
+import Posts from '../models/posts.entity';
 
 let postRouter = express.Router();
 
 /* Posts Data - Use data from jsonPlaceHolder */
 let data = {
     posts: [
-        {id: 1 , userId: 1, title: 'sunt aut facere ', body: 'quia et suscipit suscipit recusandae consequuntur expedita'},
+        {id: 1 , userId: 1, title: 'sunt aut facere', body: 'quia et suscipit suscipit recusandae consequuntur expedita'},
         {id: 2 , userId: 1, title: 'qui est esse', body: 'est rerum tempore vitae sequi sint nihil reprehenderit dolor'},
         {id: 3 , userId: 1, title: 'ea molestias quasi exercitationem', body: 'et iusto sed quo iure voluptatem occaecati omnis'}
     ]
 }
 
 /* Implement Methods */
-postRouter.get('/', (req, res) => {
-    res.json(data.posts);
+postRouter.get('/', async (req, res) => {
+    /* res.json(data.posts); */
+    let [post, error] =  await handleAsync( getRepository(Posts).find());
+    if (error) return res.send(error);
+    res.send(post);
 });
 
 postRouter.get('/:id', (req, res) => {
@@ -21,8 +27,17 @@ postRouter.get('/:id', (req, res) => {
     res.json(post[0]);
 });
 
-postRouter.post('/', (req, res) => {
-    res.json('Creating');
+
+
+postRouter.post('/', async (req, res) => {
+    /* res.json('Creating'); */
+    let data = req.body;
+    const newPost = getRepository(Posts).create(data);
+    let [post, error] =  await handleAsync( getRepository(Posts).save(newPost));
+
+    if (error) return res.send(error);
+    res.send(post);
+
 });
 
 postRouter.patch('/', (req, res) => {
@@ -32,5 +47,11 @@ postRouter.patch('/', (req, res) => {
 postRouter.delete('/', (req, res) => {
     res.json('Deleting....');
 });
+
+const handleAsync = (promise: Promise<any>) =>{
+    return promise
+        .then( (data: any) => ([data, null]) )
+        .catch( (error: any) => ([null, error]) )
+}
 
 export default postRouter;
--- 2021-04-26/express-app/src/server.ts
+++ 2021-04-26/express-app/src/server.ts
@@ -2,10 +2,34 @@ import express from 'express';
 import postRouter from './routes/posts';
 import userRouter from './routes/users';
 import cors from 'cors';
+/* Read enviroment variables */
+import 'dotenv/config';
+import {ConnectionOptions, createConnection} from 'typeorm';
+import config from './ormconfig';
 
 
 const server = express();
-const port = 3500;
+/* const port = 3500; */
+const port = process.env.SERVER_PORT;
+
+
+/* Connect To Database */
+async function connectToPersistance(){
+    try{
+        await createConnection(config);
+        console.log(`Persistance layer Connected...`);
+    }catch(error){
+        console.log(`Persistance layer Connection Failded: `, error);
+        return error;
+    }
+    
+}
+
+server.use(express.urlencoded());
+server.use(express.json());
+
+/* Call function */
+connectToPersistance();
 
 
 /* Set cors headers */
@@ -31,6 +55,12 @@ server.get('/info', (req, res) => {
     res.send('Greetings!');
 });
 
+/* Error handling */
+server.use((error: any, req: any, res: any, next: any) => {
+    console.log(`Unhandled Error: `, error);
+    res.send(`Unhandled Error, Please Try Again`);
+});
+
 server.listen(port, () => {
     console.log(`Server Running at http://127.0.0.1:${port}`);
 });
@@ -39,4 +69,8 @@ server.listen(port, () => {
 /* 
     install cors - npm i cors - @types/cors 
     import cors from 'cors'
+
+    2021-04-29
+    install typeorm
+
 */
