a37291daeb811f673d0044db6675ddd08a6a95f3
davidTorresTrejo <Luis.Torres@ejamerica.com>
2021-05-01 11:38:44 -0500

    2021-05-01 Express-App Add Crud Operations
    Add Crud Operations for Posts & Users
    Use these methods to show, insert, delete and update data.

--- 2021-04-26/express-app/src/routes/users.ts
+++ 2021-04-26/express-app/src/routes/users.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Users from '../models/users.entity';
 
 let userRoute = express.Router();
 
-/* Posts Data - Use data from jsonPlaceHolder */
+/* Users Data - Use data from jsonPlaceHolder */
 let data = {
     users: [
         {name: 'Leanne Graham', email: 'Sincere@april.biz', address:{city: 'Gwenborough'}, phone: '1-770-736-8031 x56442', company: {name: 'Romaguera-Crona'}},
@@ -11,21 +13,85 @@ let data = {
     ]
 }
 
-/* Implement Methods */
-userRoute.get('/', (req, res) => {
-    res.json(data.users);
+/* Implement Methods of CRUD Operations */
+
+/* Read all Users */
+userRoute.get('/', async (req, res) => {
+    /* res.json(data.posts); */
+    let [user, error] = await handleAsync(getRepository(Users).find());
+    if (error) return res.send(error);
+    res.send(user);
 });
 
-userRoute.post('/', (req, res) => {
-    res.json('Creating in Users');
+/* Read a single User */
+userRoute.get('/:id', async (req, res) => {
+
+    let id = req.params.id;
+    let [user, error] = await handleAsync(getRepository(Users).findOne(id));
+    if (error) return res.send(error);
+
+    if (user) {
+        res.send(user);
+    } else {
+        res.send(`No post found for id:  ${id}!`);
+    }
 });
 
-userRoute.patch('/', (req, res) => {
-    res.json('Patching in Users');
+
+/* Create a User */
+userRoute.post('/', async (req, res) => {
+    /* res.json('Creating'); */
+    let data = req.body;
+    const newUser = getRepository(Users).create(data);
+    let [user, error] = await handleAsync(getRepository(Users).save(newUser));
+
+    if (error) return res.send(error);
+    res.send(user);
+
 });
 
-userRoute.delete('/', (req, res) => {
-    res.json('Deleting in Users');
+/* Update a single User based on id */
+userRoute.patch('/:id', async (req, res) => {
+
+    const id = req.params.id;
+    const data = req.body;
+
+    let [response, error] = await handleAsync(getRepository(Users).update(id, data));
+    if (error) return res.send(error);
+
+    let [updateUser, error2] = await handleAsync(getRepository(Users).findOne(id));
+    if (error) return res.send(error2);
+
+    if (updateUser) {
+        res.send(updateUser);
+    } else {
+        res.send(`No post found for id:  ${id}!`);
+    }
+
 });
+
+
+/* Delete a single User based on id */
+userRoute.delete('/:id', async (req, res) => {
+
+    const id = req.params.id;
+
+    let [response, error] = await handleAsync(getRepository(Users).delete(id));
+    if (error) return res.send(error);
+
+    if (response.affected === 1) {
+        res.send({ deleted: true });
+    } else {
+        res.send(`No post found for id:  ${id}!`);
+    }
+
+});
+
+/* handleAsync Method */
+const handleAsync = (promise: Promise<any>) => {
+    return promise
+        .then((data: any) => ([data, null]))
+        .catch((error: any) => ([null, error]))
+}
 
 export default userRoute;
